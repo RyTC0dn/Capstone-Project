4092bfc348b033d7755f60e617c9f0e1969140c2
COdinRyT <rthompso96@gmail.com>
2025-09-22 01:36:34 -0400

    Getting new tile map and AI configuration

--- Metroidvania_Game/Assets/Scripts/EnemyAI/BasicEnemyHealth.cs
+++ Metroidvania_Game/Assets/Scripts/EnemyAI/BasicEnemyHealth.cs
@@ -1,24 +1,39 @@
+using TMPro;
 using UnityEngine;
 
 public class BasicEnemyHealth : MonoBehaviour
 {
     public int enemyHealth = 2;
+    public TextMeshProUGUI healthTrackerText;
+    public GameObject healthTrackerPrefab;
+    private float textOffset = 1;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnDeath();
+        DisplayHitTracker();
     }
 
     private void OnDeath()
     {
+        if(enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
+    private void DisplayHitTracker()
+    {
+        healthTrackerText.text = enemyHealth.ToString();
+        healthTrackerPrefab.transform.position = transform.position * textOffset;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
