e925e9c41b880fb8137a5b7fc0845fb2564a741d
TAB-211 <tristanbrown211@gmail.com>
2025-10-01 11:48:50 -0400

    Basic Axe throwing is implemented

--- Metroidvania_Game/Assets/Scripts/Character Inputs/AxeLogic.cs
+++ Metroidvania_Game/Assets/Scripts/Character Inputs/AxeLogic.cs
@@ -4,12 +4,15 @@ public class AxeLogic : MonoBehaviour
 {
 
     [SerializeField]
-    float speed = 1.0f;
+    float vSpeed = 1.0f;
     [SerializeField]
+    float hSpeed = 1.0f;
+    [SerializeField]
+    Rigidbody2D rb2D;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb2D.linearVelocity = (transform.right * vSpeed) + (transform.up *hSpeed);
     }
 
     // Update is called once per frame
