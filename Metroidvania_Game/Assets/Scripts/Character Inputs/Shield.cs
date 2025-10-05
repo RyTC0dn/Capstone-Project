0263de15ec1ad6b630f4d4c2bbf359f5f4600c5a
TAB-211 <tristanbrown211@gmail.com>
2025-10-05 16:35:03 -0400

    Setting up Shield Script

---
+++ Metroidvania_Game/Assets/Scripts/Character Inputs/Shield.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private bool isShieldUp;
+
+    public PrototypePlayerMovementControls movementControls;
+
+    public GameObject shieldCollider;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isShieldUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
