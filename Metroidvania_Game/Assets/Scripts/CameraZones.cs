45963fa6d340615e142449594dcb1d6a225ea119
COdinRyT <rthompso96@gmail.com>
2025-10-21 17:11:00 -0400

    Added camera zone gizmo

--- Metroidvania_Game/Assets/Scripts/CameraZones.cs
+++ Metroidvania_Game/Assets/Scripts/CameraZones.cs
@@ -17,4 +17,31 @@ public class CameraZones : MonoBehaviour
             }
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        //Drawing rectangle boundary that the camera moves in
+
+        Gizmos.color = new Color(255, 0, 0);
+
+        //Top line
+        Vector2 topLeft = new Vector2(minX, maxY);
+        Vector2 topRight = new Vector2(maxX, maxY);
+        Gizmos.DrawLine(topLeft, topRight);
+
+        //Bottom line
+        Vector2 bottomLeft = new Vector2(minX, minY);
+        Vector2 bottomRight = new Vector2(maxX, minY);
+        Gizmos.DrawLine(bottomLeft, bottomRight);
+
+        //Left line
+        Vector2 leftTop = new Vector2(minX, maxY);
+        Vector2 leftBottom = new Vector2(minX, minY);
+        Gizmos.DrawLine(leftTop, leftBottom);
+
+        //Right line
+        Vector2 rightTop = new Vector2(maxX, maxY);
+        Vector2 rightBottom = new Vector2(maxX, minY);
+        Gizmos.DrawLine(rightTop, rightBottom);
+    }
 }
