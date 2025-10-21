25c72dfe71f198b9ce2b593a45f8436ecb636d13
TAB-211 <tristanbrown211@gmail.com>
2025-10-21 15:46:55 -0400

    Update SpikeLogic.cs

--- Metroidvania_Game/Assets/Scripts/Environment/SpikeLogic.cs
+++ Metroidvania_Game/Assets/Scripts/Environment/SpikeLogic.cs
@@ -6,6 +6,12 @@ public class SpikeLogic : MonoBehaviour
     [SerializeField] int damage = 1;
     PrototypePlayerMovementControls playerControls;
 
+
+    private void Start()
+    {
+        playerControls = FindAnyObjectByType<PrototypePlayerMovementControls>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
