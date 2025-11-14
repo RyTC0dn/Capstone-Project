6a651183bc960965d5f5fb85e36771a318190f05
COdinRyT <rthompso96@gmail.com>
2025-11-14 16:16:22 -0500

    Revert "Merge branch 'Ryan/PlaytestBuild2.2'"
    This reverts commit 975f0a61de50dd19e49623fefd26009d145cd7a9, reversing
    changes made to e5b04b11acb66730ad832d0fa0a375edaabd9f7a.

--- Metroidvania_Game/Assets/Scripts/Character Inputs/AxeLogic.cs
+++ Metroidvania_Game/Assets/Scripts/Character Inputs/AxeLogic.cs
@@ -10,10 +10,6 @@ public class AxeLogic : MonoBehaviour
     float hSpeed = 1.0f;
     [SerializeField]
     float attackRate = 1.0f;
-    [SerializeField]
-    int damage = 1;
-
-    public GameEvent attackEnemyEvent;
 
 
     private float nextTimeToAttack;
@@ -33,30 +29,16 @@ public class AxeLogic : MonoBehaviour
     {
         if (collision.CompareTag("GroundEnemy"))
         {
-            attackEnemyEvent.Raise(this, new AttackDataAxe(collision.gameObject, damage));
-            Destroy(gameObject);            
+            Destroy(gameObject);
         }
 
         if (collision.CompareTag("FlyingEnemy"))
         {
-            attackEnemyEvent.Raise(this, new AttackDataAxe(collision.gameObject, damage));
-            Destroy(gameObject);            
+            Destroy(gameObject);
         }
 
         if (collision.CompareTag("Environment")) { Destroy(gameObject); }
 
     }
 
-}
-
-public class AttackDataAxe //Just for setting the data that will damage enemies
-{
-    public GameObject target;
-    public int damage;
-
-    public AttackDataAxe(GameObject target, int damage)
-    {
-        this.target = target;
-        this.damage = damage;
-    }
 }
