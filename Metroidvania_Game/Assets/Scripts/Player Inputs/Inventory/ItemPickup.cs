af0a691619bc68c5b758814a2f8a2911529611ad
TAB-211 <tristanbrown211@gmail.com>
2025-11-27 13:36:01 -0500

    Update ItemPickup.cs

--- Metroidvania_Game/Assets/Scripts/Player Inputs/Inventory/ItemPickup.cs
+++ Metroidvania_Game/Assets/Scripts/Player Inputs/Inventory/ItemPickup.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(Rigidbody2D))] //Use kinematic 
 public class ItemPickup : MonoBehaviour
 {
     public GameEvent abilityPickup;
     private bool hasPickedUpShield = false;
     private bool hasPickedUpWallBreak = false;
+    public GameObject itemShield; //Manually assign in inpsector
+    public GameObject itemWallBreak; //Manually assign in inpsector
 
     public SceneInfo sceneInfo;
 
     private void Start()
     {
-        if(sceneInfo.isShieldPickedUp && gameObject.name == "ShieldPickup")
+        if (sceneInfo.isShieldPickedUp && gameObject.name == "ShieldPickup")
             Destroy(gameObject);
         if (sceneInfo.isWallBreakPickedUp && gameObject.name == "WallBreakPickup")
             Destroy(gameObject);
@@ -19,13 +22,13 @@ public class ItemPickup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player" && gameObject.tag == "AbilityPickup" && gameObject.name == "ShieldPickup")
+        if (collision.tag == "Player" && gameObject.tag == "AbilityPickup" && this.gameObject == itemShield)
         {
             hasPickedUpShield = true;
             abilityPickup.Raise(this, hasPickedUpShield);
             Destroy(gameObject);
         }
-        if (collision.tag == "Player" && gameObject.tag == "AbilityPickup" && gameObject.name == "WallBreakPickup")
+        if (collision.tag == "Player" && gameObject.tag == "AbilityPickup" && this.gameObject == itemWallBreak)
         {
             hasPickedUpWallBreak = true;
             abilityPickup.Raise(this, hasPickedUpWallBreak);
