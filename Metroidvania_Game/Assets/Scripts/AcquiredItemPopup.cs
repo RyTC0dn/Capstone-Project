a0386f661f9edf63f20f578e283fa8fd2b02d573
COdinRyT <rthompso96@gmail.com>
2025-11-27 15:03:39 -0500

    Add item pickup popup UI and update ability info
    Introduced a new item pickup popup UI to the ShieldPickup prefab, including ability name and image display. Updated AcquiredItemPopup script to set UI elements and hide the popup by default. Changed AbilityInformation to use Sprite instead of Image for ability images. Enabled root motion on the player character's Animator. Removed animation event from KnightJumpAnim.

--- Metroidvania_Game/Assets/Scripts/AcquiredItemPopup.cs
+++ Metroidvania_Game/Assets/Scripts/AcquiredItemPopup.cs
@@ -1,20 +1,25 @@
+using TMPro;
+using UnityEditor.U2D;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AcquiredItemPopup : MonoBehaviour
 {
     public AbilityInformation abilityItem;
+    public GameObject pickupObjectCanvas;
+    public TextMeshProUGUI itemAbilityName;
+    public Image itemAbilityImage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        itemAbilityImage.sprite = abilityItem.abilityImage;
+        itemAbilityName.text = abilityItem.abilityName;
+        pickupObjectCanvas.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void WhenPickedUp(Component sender, object data)
     {
-        
+
     }
-
-
 }
