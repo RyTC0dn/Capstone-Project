684efaf4c44ecbd9442b85de86b5534e40436412
COdinRyT <rthompso96@gmail.com>
2026-01-13 12:41:30 -0500

    ai update

--- Metroidvania_Game/Assets/Scripts/EnemyAI/Rat_Enemy_AI_Logic.cs
+++ Metroidvania_Game/Assets/Scripts/EnemyAI/Rat_Enemy_AI_Logic.cs
@@ -1,57 +1,56 @@
-using System.Xml.Serialization;
 using Unity.AppUI.UI;
-using Unity.Behavior;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Rat_Enemy_AI_Logic : MonoBehaviour
 {
     [Header("References")]
     private Transform playerTransform;
+    private Rigidbody2D rb2D;
     private Animator animator;
     private Vector2 lastPos;
     private float horizontal;
     private float h;
-    public static Rat_Enemy_AI_Logic Instance { get {  return Instance; } }
 
     [Header("Layers")]
     [SerializeField] private LayerMask playerLayer;
-    [SerializeField]private LayerMask obstructLayer;
+    [SerializeField] private LayerMask obstructLayer;
     [Space(20)]
 
     [Header("Patrol Settings")]
-    [SerializeField]private float enemySpeed;
-    [SerializeField]private GameObject[] currentWaypoint;
+    [SerializeField] private float enemySpeed;
+    [SerializeField] private GameObject[] currentWaypoint;
     private int waypointIndex;
     private bool patrolling = true;
+    [Space(20)]
 
     [Header("Combat Settings")]
     [SerializeField] private int attackDamage = 1;
-    [SerializeField]private float attackCooldown = 1f;
+    [SerializeField] private float attackCooldown = 1f;
     public GameEvent onAttackEvent;
     public Collider2D attackCollider;
-    [SerializeField]private float attackRadius;
+    [SerializeField] private float launchDistance;
+    [SerializeField] private float launchHeight;
+    private bool canAttack = false;
     [Space(20)]
 
     [Header("Detection Ranges")]
-    [SerializeField]private float visionRange; //How far the AI can see 
+    [SerializeField] private float visionRange; //How far the AI can see 
     [SerializeField] private float fovAngle; //The vision angle 
-    [SerializeField]private float attackRange;
-    [SerializeField] private float topPoint;
-    [SerializeField] private float bottomPoint;
-    private bool isPlayerInSight;
+    [SerializeField] private float attackRange;
+    private bool isPlayerInSight = false;
     private bool isPlayerInRange = false;
     [Range(0, 1)]
     public float detectorTransparency;
 
     [Tooltip("Should be the same as the death animation run time")]
-    [SerializeField]private float deathTimer;
+    [SerializeField] private float deathTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
         attackCollider.enabled = false;
     }
 
@@ -60,10 +59,9 @@ public class Rat_Enemy_AI_Logic : MonoBehaviour
     {
         //Detection logic functions
         DetectPlayer();
-        AttackRange();
 
         //State logic functions
-        if (patrolling && !isPlayerInSight || patrolling && !isPlayerInRange)
+        if (patrolling && !isPlayerInSight)
         {
             Patrolling();
         }
@@ -71,7 +69,7 @@ public class Rat_Enemy_AI_Logic : MonoBehaviour
         {
             ChasePlayer();
         }
-        else if(!patrolling && isPlayerInRange)
+        else if (!patrolling && isPlayerInRange)
         {
             AttackPlayer();
         }
@@ -85,11 +83,11 @@ public class Rat_Enemy_AI_Logic : MonoBehaviour
         horizontal = velocity.x;
 
         lastPos = currentPosition;
-        if(horizontal > 0)
+        if (horizontal > 0)
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
-        else if(horizontal < 0)
+        else if (horizontal < 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
@@ -97,15 +95,15 @@ public class Rat_Enemy_AI_Logic : MonoBehaviour
 
     private void Patrolling()
     {
-        transform.position = Vector2.MoveTowards(transform.position, currentWaypoint[waypointIndex].transform.position, 
+        transform.position = Vector2.MoveTowards(transform.position, currentWaypoint[waypointIndex].transform.position,
             enemySpeed * Time.deltaTime);
 
-        if(Vector2.Distance(transform.position, currentWaypoint[waypointIndex].transform.position) < 0.2f)
+        if (Vector2.Distance(transform.position, currentWaypoint[waypointIndex].transform.position) < 0.2f)
         {
             //go to next waypoint
             waypointIndex++;
 
-            if(waypointIndex >= currentWaypoint.Length)
+            if (waypointIndex >= currentWaypoint.Length)
             {
                 //Reset waypoint index once no more waypoints found
                 waypointIndex = 0;
@@ -122,7 +120,16 @@ public class Rat_Enemy_AI_Logic : MonoBehaviour
     {
         attackCollider.enabled = true;
 
+        canAttack = true;
+
         animator.Play("EnemyRatAttack");
+
+        Vector2 launchAtk = new Vector2(launchDistance, launchHeight);
+
+        Vector2 distanceToPlayer = (playerTransform.position - transform.position).normalized;
+
+
+        rb2D.AddForce(distanceToPlayer * launchDistance, ForceMode2D.Impulse);
 
         if (attackCollider.CompareTag("Player"))
         {
@@ -134,59 +141,52 @@ public class Rat_Enemy_AI_Logic : MonoBehaviour
 
     public void AttackRange()
     {
-        //RaycastHit2D attackRay = Physics2D.Raycast(transform.position, Vector2.left,
-        //   attackRange, playerLayer);
-
-        //if (attackRay.collider != null)
-        //{            
-        //    Debug.DrawRay(transform.position, Vector2.left * attackRange, Color.white);
-        //    isPlayerInRange = true;
-        //}
-        //else
-        //{
-        //    Color orange = new Color(255, 174, 66);
-        //    Debug.DrawRay(transform.position, Vector2.left * attackRange, orange);
-        //    isPlayerInRange = false;
-        //}
+        if (Vector2.Distance(transform.position, playerTransform.position) <= attackRange)
+            isPlayerInRange = true;
     }
 
     private void DetectPlayer()
     {
-        if(horizontal > 0)
+        if (horizontal > 0)
         {
             h = -1;
         }
-        else if(horizontal < 0)
+        else if (horizontal < 0)
         {
             h = 1;
         }
 
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.left  * new Vector2(h, 0), 
-            visionRange, playerLayer);
-
-
-        RaycastHit2D topRay = Physics2D.Raycast(transform.position, new Vector2(visionRange, visionRange * Mathf.Tan(45)));
-
-
-        //if(Physics2D.OverlapCircle(transform.position, visionRange).CompareTag("Player"))
-
         #region Detection Logic
-        if (ray.collider != null)
-        {
-            Debug.DrawRay(transform.position, Vector2.left * new Vector2(h, 0), Color.green);
+        Vector2 direction = h > 0 ? Vector2.left : Vector2.right;
+        bool playerDetected = false;
 
-                Debug.DrawRay(transform.position, new Vector2(visionRange, visionRange * Mathf.Tan(45)), Color.green);
-            if(ray.transform == playerTransform)
-            {
-                isPlayerInSight = true;
-                patrolling = false;
-                Debug.Log("Found Player");  
-            }
-            
+        Vector2 topDir = Quaternion.Euler(0, 0, fovAngle * 0.5f) * direction;
+        Vector2 bottomDir = Quaternion.Euler(0, 0, -fovAngle * 0.5f) * direction;
+
+        RaycastHit2D topRay = Physics2D.Raycast(transform.position, topDir, visionRange, playerLayer);
+        RaycastHit2D bottomRay = Physics2D.Raycast(transform.position, bottomDir, visionRange, playerLayer);
+
+        Debug.DrawRay(transform.position, topDir * visionRange, Color.yellow);
+        Debug.DrawRay(transform.position, bottomDir * visionRange, Color.yellow);
+
+
+        if (topRay != null)
+        {
+            playerDetected = true;
+        }
+        else if (bottomRay != null)
+        {
+            playerDetected = true;
+        }
+
+        if (playerDetected)
+        {
+            Debug.Log("Player Detetced");
+            patrolling = false;
+            isPlayerInSight = true;
         }
         else
         {
-            Debug.DrawRay(transform.position, Vector2.left * new Vector2(h, 0), Color.red);
             isPlayerInSight = false;
             patrolling = true;
         }
@@ -207,7 +207,7 @@ public class Rat_Enemy_AI_Logic : MonoBehaviour
         Color detection = new Color(255, 0, 0, detectorTransparency);
 
         Gizmos.color = detection;
-        Gizmos.DrawSphere(transform.position, visionRange / 2);
+        Gizmos.DrawSphere(transform.position, attackRange / 2);
     }
 
     #region AttackActions
