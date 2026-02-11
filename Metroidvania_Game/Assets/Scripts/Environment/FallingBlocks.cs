653aca2b20a94a2afe0eab4e5fc0d1954e671e76
TAB-211 <tristanbrown211@gmail.com>
2026-02-11 13:28:00 -0500

    Beginning to create the falling blocks mechanic

---
+++ Metroidvania_Game/Assets/Scripts/Environment/FallingBlocks.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FallingBlocks : MonoBehaviour
+{
+    [SerializeField] private float fallDelay = 1.0f;
+
+    [SerializeField] private Rigidbody2D rb2D;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
