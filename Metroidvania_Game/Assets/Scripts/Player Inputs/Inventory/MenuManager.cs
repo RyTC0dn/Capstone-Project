cd928f7a76852ca8c911907f7574b5a721abf6c1
COdinRyT <rthompso96@gmail.com>
2026-02-13 03:13:42 -0500

    Added new UI menu sprites

--- Metroidvania_Game/Assets/Scripts/Player Inputs/Inventory/MenuManager.cs
+++ Metroidvania_Game/Assets/Scripts/Player Inputs/Inventory/MenuManager.cs
@@ -36,6 +36,7 @@ public class MenuManager : MonoBehaviour
     public GameObject equipmentMenu;
     public GameObject inventoryMenu;
     public GameObject questMenu;
+    public GameObject tutorialMenu;
     private PrototypePlayerAttack playerAttack; //So I can disable attack when menu is open
     [Space(20)]
 
@@ -203,6 +204,7 @@ public class MenuManager : MonoBehaviour
 
         inventoryMenu.SetActive(false);
         questMenu.SetActive(false);
+        tutorialMenu.SetActive(false);
 
         menuAudio.PlayOneShot(menuClips[0]);
     }
@@ -215,6 +217,7 @@ public class MenuManager : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(menuFirst[1]);
 
         questMenu.SetActive(false);
+        tutorialMenu.SetActive(false);
 
         menuAudio.PlayOneShot(menuClips[0]);
     }
@@ -226,6 +229,20 @@ public class MenuManager : MonoBehaviour
         questMenu.SetActive(true);
 
         EventSystem.current.SetSelectedGameObject(menuFirst[2]);
+
+        tutorialMenu.SetActive(false);
+
+        menuAudio.PlayOneShot(menuClips[0]);
+    }
+
+    public void TutorialOpen()
+    {
+        equipmentMenu.SetActive(false);
+        inventoryMenu.SetActive(false);
+        questMenu.SetActive(false);
+        tutorialMenu.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(menuFirst[3]);
 
         menuAudio.PlayOneShot(menuClips[0]);
     }
@@ -244,6 +261,8 @@ public class MenuManager : MonoBehaviour
             inventoryMenu.SetActive(false);
         else if(questMenu.activeInHierarchy)
             questMenu.SetActive(false);
+        else if(tutorialMenu.activeInHierarchy)
+            tutorialMenu.SetActive(false);
         else
         {
             //If no menus are open 
