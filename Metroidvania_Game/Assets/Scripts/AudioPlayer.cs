5ed037a00ff33f1aa8c4963a3ec960e41fc0d0f3
RyTC0dn <rthompso96@gmail.com>
2026-03-17 15:42:37 -0400

    Highlight vfx & audio player adjustments

--- Metroidvania_Game/Assets/Scripts/AudioPlayer.cs
+++ Metroidvania_Game/Assets/Scripts/AudioPlayer.cs
@@ -1,9 +1,37 @@
 using UnityEngine;
+using UnityEngine.Events;
 
+[System.Serializable]
+public class CustomAudioPlayer : UnityEvent<Component, AudioClip> { }
+
+[RequireComponent(typeof(AudioSource))]
 public class AudioPlayer : MonoBehaviour
 {
     public AudioClip[] clips; //Manually assignable array of audio clips in the inspector
+    public AudioSource audioSource;
     private static float lastAudioPlayed;
+    [Space(20)]
+
+    [Header("Play settings")]
+    public bool playOnAwake;
+
+    [Tooltip("Select one of these options to play single, random, or sequence of clips")]
+    public bool playAudio, playRandom, playCycle;
+    [Space(10)]
+
+    [SerializeField] private int minValue;
+    [SerializeField] private int maxValue;
+    public CustomAudioPlayer player;
+
+    private void Update()
+    {
+        if(playOnAwake)
+        {
+            if (playAudio) { PlayAudio(minValue, audioSource); }
+            else if(playRandom) { PlayRandomClip(audioSource, minValue, maxValue); }
+            else if(playCycle) { CycleAudioClips(audioSource); }
+        }
+    }
 
     public void PlayAudio(int clipIndex, AudioSource source)
     {
@@ -13,7 +41,8 @@ public class AudioPlayer : MonoBehaviour
             return;
         }
 
-        if (!source.isPlaying) //Prevent overlapping audio clips
+
+        if (!source.isPlaying) 
         {
             if (Time.time - AudioPlayer.lastAudioPlayed > 0.5f)
             {
@@ -42,5 +71,29 @@ public class AudioPlayer : MonoBehaviour
                 source.PlayOneShot(source.clip);
             }
         }
+    }
+
+    public void CycleAudioClips(AudioSource source)
+    {
+        // Ensure that the clips array is not empty and that the provided range is valid
+        if (clips.Length == 0)
+        {
+            Debug.LogWarning("No audio clips assigned to the AudioPlayer.");
+            return;
+        }
+
+        if (!source.isPlaying)
+        {
+            if(Time.time - AudioPlayer.lastAudioPlayed > 0.5f)
+            {
+                minValue = (minValue + 1) % clips.Length;
+                source.PlayOneShot(clips[minValue]);
+            }
+        }
+    }
+
+    public void OnAudioEvent(Component sender, AudioClip clip)
+    {
+        player.Invoke(sender, clip);
     }
 }
