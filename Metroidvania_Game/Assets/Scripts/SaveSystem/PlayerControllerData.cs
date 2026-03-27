c4bd586e5d7a7beffb34764755c26b6e8472f126
TAB-211 <tristanbrown211@gmail.com>
2026-03-27 12:19:24 -0400

    Update PlayerControllerData.cs

--- Metroidvania_Game/Assets/Scripts/SaveSystem/PlayerControllerData.cs
+++ Metroidvania_Game/Assets/Scripts/SaveSystem/PlayerControllerData.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 [System.Serializable]
 public class PlayerControllerData
 {
-    public int health;
-
     public float[] position;
 
     public PlayerControllerData (PrototypePlayerMovementControls controller) 
